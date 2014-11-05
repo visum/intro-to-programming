8dd935ba3119eeaacc239f5a2448b38fdd74e2bc
Benjamin Howe <bdhowe@gmail.com>
2014-11-04 20:11:59 -0700

    Improvement to the first console page

--- components/app/TwoConsole.js
+++ components/app/TwoConsole.js
@@ -7,17 +7,24 @@ BASE.require([
   app.TwoConsole = function(elem, tags, scope){
     var self = this;
     components.ui.states.UIStateBehavior.call(self);
+    var slider = $(tags['slider']).controller();
+    var $next = $(tags['next']);
+
+    var sliderStates = ["two", "three"];
 
     self.stateActive = function(){
       console.log("/*****************************************/");
       console.log("/*                                       */");
       console.log("/* Hey! Over here!  This is the console! */");
-      console.log("/* If you can see me, type \"hi()\" and    */");
-      console.log("/* press enter.                          */");
+      console.log("/* If you can see me, type               */");
+      console.log("/* hi()                                  */");
+      console.log("/* here in the console and               */");
+      console.log("/* press enter/return.                   */");
       console.log("/*                                       */");
       console.log("/*****************************************/");
 
       window.hi = function(){
+        console.log("Good job! Now, look back at the main window.");
         self.stateManager.replace("console-2");
       };
 
@@ -27,6 +34,13 @@ BASE.require([
       delete window.hi;
       return new BASE.async.Future.fromResult();
     };
+
+    $next.on("click", function(){
+      slider.replace(sliderStates.shift());
+      if (sliderStates.length === 0) {
+        $next.addClass("hide");
+      }
+    });
 
   };
 
